984700a1d893ef0678db4936243cbb67490635ff
adistroe <97851663+adistroe@users.noreply.github.com>
2023-04-28 22:39:56 +0300

    Delete Connect Four/task/src/connectfour directory

--- Connect Four/task/src/connectfour/Main.kt
+++
@@ -1,5 +0,0 @@
-package connectfour
-
-fun main() {
-    Game().run()
-}
--- Connect Four/task/src/connectfour/Player.kt
+++
@@ -1,8 +0,0 @@
-package connectfour
-
-data class Player(val name: String, val glyph: Glyph, var score: Int = 0) {
-    enum class Glyph(val symbol: Char) {
-        FIRST_PLAYER('o'),
-        SECOND_PLAYER('*')
-    }
-}
